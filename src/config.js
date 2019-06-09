51045c8faea0c6719cbc13061ac971557fc5618b
David G <david.garcia@scmspain.com>
2019-06-09 20:44:32 +0200

    updates token

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+// renew expired auth token: https://developer.spotify.com/console/get-new-releases/
+
 export const SPOTIFY_API = {
   new_releases_url: "https://api.spotify.com/v1/browse/new-releases",
   categories_url: "https://api.spotify.com/v1/browse/categories",
@@ -6,5 +8,5 @@ export const SPOTIFY_API = {
   limit: 9,
   offset: 5,
   authorization:
-    "Bearer BQAGC85FzEr4k6SRj7VzQYDY-gbj7Ta0Gqg9ypXFkcd4esKcyexSGLYOngFSbk1fg9uLsU0Oipo3gENNsiX27bdJmDL_MfIkZmP-am3sRTvWr2Dqzq9OVHjvSSWpgMBUZRLj3D2PjNwHmKphSw"
+    "Bearer BQCHMZexLASxMw1FTFtPzZKFXWb3SHu4u5WYVMofRQ6IEEH1nIE6D1lDo1TlPUF4SJLNEnUDkA_mYau-QTZXYSNyjBlBWeZDfeJEgYTfjwaJj6NVygUsWCObgE2Anst28tkLWFqUM3rQMh7cFg"
 };
