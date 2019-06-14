6948620990dc182b9baf93ac85635d8e8350f136
David G <david.garcia@scmspain.com>
2019-06-14 12:35:11 +0200

    adds Homepage to router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigation } from "./Navigation";
+import { HomePage } from "./HomePage";
 import { NewReleasesList } from "./NewReleasesList";
 import { CategoriesList } from "./CategoriesList";
 import { NewReleasesArtist } from "./NewReleasesArtist";
@@ -14,7 +15,8 @@ export const App = () => (
       <h1 className="page-heading">Spotify Explorer</h1>
     </section>
     <section className="page-layout">
-      <Route path="/" exact component={NewReleasesList} />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/new-releases" component={NewReleasesList} />
       <Route path="/categories" component={CategoriesList} />
       <Route
         path="/album/:artist/:idArtist"
