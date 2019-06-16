d48325619b109ebcd4a2828a12858db5f632d29f
David G <david.garcia@scmspain.com>
2019-06-16 15:08:47 +0200

    more info at newReleases component

--- src/components/NewReleasesItem.js
+++ src/components/NewReleasesItem.js
@@ -3,9 +3,21 @@ import { Link } from "react-router-dom";
 
 const baseClass = "newReleases";
 
-export const NewReleasesItem = ({ artist, cover, idArtist }) => (
+export const NewReleasesItem = ({
+  album,
+  artist,
+  cover,
+  idArtist,
+  spotifyLink
+}) => (
   <div className={baseClass}>
-    <h3>{artist}</h3>
+    <h2>
+      {artist}: <span className={`${baseClass}-albumName`}>{album}</span>
+    </h2>
+    <a
+      className={`${baseClass}-link`}
+      href={spotifyLink}
+    >{`Listen to ${artist} in Spotify`}</a>
     <Link to={`/album/${artist}/${idArtist}`}>
       <img className={`${baseClass}-item`} src={cover} />
     </Link>
