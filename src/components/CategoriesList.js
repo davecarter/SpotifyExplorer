d4a37bcfff8fa580e3126c33700b0f1d2e5c0494
David G <david.garcia@scmspain.com>
2019-06-26 18:56:38 +0200

    removes mocked API response

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// import { client, SPOTIFY_API } from "../domain/spotify";
-import { categoriesMock } from "../domain/categoriesMock";
+import { client, SPOTIFY_API } from "../domain/spotify";
+// import { categoriesMock } from "../domain/categoriesMock";
 
 export const CategoriesList = () => {
-  // const { categories_url, country, locale, limit, offset } = SPOTIFY_API;
+  const { categories_url, country, locale, limit, offset } = SPOTIFY_API;
   const [categoriesList, setCategoriesList] = useState([]);
   useEffect(() => {
-    // const categoriesListUrl = `${categories_url}?country=${country}&locale=${locale}&limit=${limit}&offset=${offset}`;
-    // client(categoriesListUrl)
-    //   .then(data => setCategoriesList(data.categories.items))
-    //   .catch(err => console.log(err));
-    setCategoriesList(categoriesMock.categories.items);
+    const categoriesListUrl = `${categories_url}?country=${country}&locale=${locale}&limit=${limit}&offset=${offset}`;
+    client(categoriesListUrl)
+      .then(data => setCategoriesList(data.categories.items))
+      .catch(err => console.log(err));
+    // setCategoriesList(categoriesMock.categories.items);
   }, []);
 
   const renderCategoriesList = () => {
--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { SPOTIFY_API } from "../domain/spotify";
+import React, { useEffect, useState } from "react";
+import { client, SPOTIFY_API } from "../domain/spotify";
 import { categoryMock } from "../domain/categoryMock";
 
 export const CategoryPage = ({ match }) => {
+  const [categoryImage, setCategoryImage] = useState("");
   const { categories_url } = SPOTIFY_API;
   const title = match.params.id.toUpperCase();
-  // {`${categories_url}/${match.params.id}`}
+  const category_url = `${categories_url}/${match.params.id}`;
+  console.log("URL", category_url);
+  useEffect(() => {
+    client(category_url).then(data => setCategoryImage(data.icons[0]));
+  });
   return (
     <div className="categoryPage">
       <h1 className="categoryPage-title">{title}</h1>
-      <img className="categoryPage-image" src={categoryMock.icons[0].url} />
+      <img className="categoryPage-image" src={categoryImage.url} />
     </div>
   );
 };
